16621f5a553ef5fd7a957ac61108232e6a84e73e
sheltowt <sheltowt@gmail.com>
2016-09-03 09:17:19 -0400

    updates

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var port = process.env.PORT || 8080;
+
+
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/bower_components'));
+
+
+app.use(bodyParser.json()); 
+
+
+app.listen(port);
+console.log("app running on port " + port)	
+exports = module.exports = app; 	
